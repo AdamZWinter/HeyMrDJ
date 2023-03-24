bec1b8fedb8add2cfc89c5f7c3804beea6abfd6d
AdamZWinter <adam@bootstrapmusic.com>
2023-03-24 12:01:18 -0400

    json format playlist for adding songs to database

--- database/playlists.sql
+++ database/playlists.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `playlists` (
     `id` int(32) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `songs` varchar(16384) NOT NULL
-)
+);
 
 
 CREATE TABLE `requestlists` (
                                 `id` int(32) NOT NULL PRIMARY KEY AUTO_INCREMENT,
                                 `songs` varchar(16384) NOT NULL
-)
+);
